11155e9444e9e24860555517951717552e4d91d1
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-07 22:29:28 +0100

    Updated Brand repository

--- backend/Repositories/BrandRepository.cs
+++ backend/Repositories/BrandRepository.cs
@@ -6,10 +6,45 @@ namespace backend.Repositories
 {
     public class BrandRepository
     {
-        public List<Brand> get()
+        public List<Brand> Get()
         {
             using (var db = new DatabaseContext())
                 return db.Brands.ToList();
+        }
+
+        public Brand Get(int id)
+        {
+            using (var db = new DatabaseContext())
+                return db.Brands.Find(id);
+        }
+
+        public Brand Create(Brand brand)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var result = db.Brands.Add(brand);
+                db.SaveChanges();
+                return result.Entity;
+            }
+        }
+
+        public Brand Update(Brand brand)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var updated = db.Brands.Update(brand);
+                db.SaveChanges();
+                return updated.Entity;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Brands.Remove(Get(id));
+                db.SaveChanges();
+            }
         }
     }
 }
