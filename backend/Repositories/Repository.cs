27925c2f13647076a653aa7a1429850b9d52180d
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-07 23:04:51 +0100

    For now an un used Repository, the idea is to have something generic, to avoid to have to duplicate all the CRUD code

---
+++ backend/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories
+{
+    public class Repository<T> where T : BaseEntity
+    {
+        private readonly DatabaseContext context;
+        private DbSet<T> entities;
+        string errorMessage = string.Empty;
+
+        public Repository(DatabaseContext context)
+        {
+            this.context = context;
+            entities = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public T Get(long id)
+        {
+            return entities.SingleOrDefault(s => s.Id == id);
+        }
+        public void Insert(T entity)
+        {
+            entities.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Update(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            entities.Remove(entity);
+            context.SaveChanges();
+        }
+    }
+}
