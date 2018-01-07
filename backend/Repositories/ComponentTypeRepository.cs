ac9551eb1d26ce5e4e3aaf9dbd8e0659512c0a8c
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-07 22:40:26 +0100

    Added component type repository

---
+++ backend/Repositories/ComponentTypeRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using backend.Models;
+
+namespace backend.Repositories
+{
+    public class ComponentTypeRepository
+    {
+        public List<ComponentType> Get()
+        {
+            using (var db = new DatabaseContext())
+                return db.ComponentTypes.ToList();
+        }
+
+        public ComponentType Get(int id)
+        {
+            using (var db = new DatabaseContext())
+                return db.ComponentTypes.Find(id);
+        }
+
+        public ComponentType Create(ComponentType type)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var result = db.ComponentTypes.Add(type);
+                db.SaveChanges();
+                return result.Entity;
+            }
+        }
+
+        public ComponentType Update(ComponentType type)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var updated = db.ComponentTypes.Update(type);
+                db.SaveChanges();
+                return updated.Entity;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.ComponentTypes.Remove(Get(id));
+                db.SaveChanges();
+            }
+        }
+    }
+}
