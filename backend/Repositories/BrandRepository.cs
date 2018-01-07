c8d8bd3b27f55e9130979e48e88b65c82d5ab949
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-07 21:13:57 +0100

    Created Brand and component repositories with just the get method

---
+++ backend/Repositories/BrandRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using backend.Models;
+
+namespace backend.Repositories
+{
+    public class BrandRepository
+    {
+        public List<Brand> get()
+        {
+            using (var db = new DatabaseContext())
+                return db.Brands.ToList();
+        }
+    }
+}
---
+++ backend/Repositories/ComponentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories
+{
+    public class ComponentRepository
+    {
+        public List<Component> get()
+        {
+            using (var db = new DatabaseContext())
+            {
+                // Forcing eager loading on foreign tables
+                db.Components.Include(c => c.Type).Load();
+                db.Components.Include(c => c.Brand).Load();
+                return db.Components.ToList();
+            }
+        }
+    }
+}
