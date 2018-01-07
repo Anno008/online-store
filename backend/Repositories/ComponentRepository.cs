336db826b77f035cfcb5bbda842bef72c7fbf23c
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-07 22:36:30 +0100

    Updated component repository

--- backend/Repositories/ComponentRepository.cs
+++ backend/Repositories/ComponentRepository.cs
@@ -7,7 +7,7 @@ namespace backend.Repositories
 {
     public class ComponentRepository
     {
-        public List<Component> get()
+        public List<Component> Get()
         {
             using (var db = new DatabaseContext())
             {
@@ -15,6 +15,46 @@ namespace backend.Repositories
                 db.Components.Include(c => c.Type).Load();
                 db.Components.Include(c => c.Brand).Load();
                 return db.Components.ToList();
+            }
+        }
+
+        public Component Get(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                // Forcing eager loading on foreign tables
+                db.Components.Include(c => c.Type).Load();
+                db.Components.Include(c => c.Brand).Load();
+                return db.Components.Find(id);
+            }
+        }
+
+        public Component Create(Component component)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var result = db.Components.Add(component);
+                db.SaveChanges();
+                return result.Entity;
+            }
+        }
+
+        public Component Update(Component component)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var updated = db.Components.Update(component);
+                db.SaveChanges();
+                return updated.Entity;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Components.Remove(Get(id));
+                db.SaveChanges();
             }
         }
     }
