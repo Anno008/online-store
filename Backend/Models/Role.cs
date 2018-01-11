764076d66a6a6a3467fc218a5b979eba5f172d6c
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-11 19:16:30 +0100

    Created User and Role models

---
+++ Backend/Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Models
+{
+    public enum Role
+    {
+        User,
+        Admin
+    }
+}
---
+++ Backend/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
