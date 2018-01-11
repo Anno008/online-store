a5b17f3fd0932dbacd1deeaa5d1e19343b3490f2
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-11 21:01:42 +0100

    Created user controller

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Backend.DTOs;
+using Backend.Services;
+using System.Net.Http;
+using System.Net;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    //[Authorize(AuthenticationSchemes = "Bearer")]
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService) =>
+            _userService = userService;
+
+        [HttpPost]
+        [ActionName("login")]
+        public IActionResult Login(UserDTO user)
+        {
+            var result = _userService.Login(user.Username, user.Password);
+            if (result == null)
+                return BadRequest("Incorrect credentials.");
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ActionName("register")]
+        public IActionResult Register(UserDTO user)
+        {
+            var result = _userService.Register(user.Username, user.Password);
+            if (result != null)
+                return Ok(result);
+
+            return BadRequest("The user with the given username already exists.");
+        }
+
+        [HttpGet]
+        [ActionName("test")]
+        public IActionResult Test()
+        {
+            //return "Super secret content, I hope you've got clearance for this...";
+            return StatusCode(401, "What are you");
+        }
+    }
+}
