b18531d2f15c43e357db3bbcbf71604f8e2ad366
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-11 21:00:07 +0100

    Updated Startup. Added repositories as dependencies.

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Backend.Repositories;
+using Backend.Services;
+using Backend.Services.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -10,14 +14,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Backend
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -25,8 +35,21 @@ namespace Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Enable CORS requests
+            services.AddCors();
+
+            services.Configure<JWTSettings>(Configuration.GetSection("JWTSettings"));
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => GetTokenValidationParameters());
+
             services.AddMvc();
+
             services.AddDbContext<DatabaseContext>();
+            services.AddTransient<UserService>();
+            services.AddTransient<UserRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,15 +60,41 @@ namespace Backend
                 app.UseDeveloperExceptionPage();
             }
 
+            // handle exceptions globally
+            AppDomain.CurrentDomain.UnhandledException += HandleErrors;
+
+            // configure CORS
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
             app.UseMvc();
-
-            // Testing
-            var brandRepo = new BrandRepository();
-            var bResult = brandRepo.Get();
-
-            var compRepo = new ComponentRepository();
-            var cResultr = compRepo.Get();
-
         }
+
+        private void HandleErrors(object sender, UnhandledExceptionEventArgs args)
+        {
+            if (sender is Exception)
+            {
+                Console.WriteLine((sender as Exception).Message);
+            }
+        }
+
+        private TokenValidationParameters GetTokenValidationParameters() =>
+          new TokenValidationParameters
+          {
+              // The signing key must match!
+              ValidateIssuerSigningKey = true,
+              // Validate the JWT Issuer (iss) claim
+              ValidateIssuer = true,
+              // Validate the JWT Audience (aud) claim
+              ValidateAudience = true,
+              ValidIssuer = Configuration.Get<JWTSettings>().Issuer,
+              ValidAudience = Configuration.Get<JWTSettings>().Audience,
+              NameClaimType = "sub",
+              // Validate the token expiry
+              ValidateLifetime = true,
+              // Amount of clock drift -
+              ClockSkew = TimeSpan.FromSeconds(30),
+              IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(Configuration.Get<JWTSettings>().SecretKey))
+          };
     }
 }
