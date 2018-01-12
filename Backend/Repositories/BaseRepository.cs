1f8f14b3b059a0eafc5b32c445d477a8ac0f7dbc
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-12 20:27:24 +0100

    Updated Repository. Renamed it to base repository and added generic methods for crud operations, both synchronous and asynchronous variants

---
+++ Backend/Repositories/BaseRepository.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repositories
+{
+    public class BaseRepository<T> where T : BaseEntity
+    {
+        private readonly DatabaseContext context;
+        private readonly DbSet<T> entities;
+        string errorMessage = string.Empty;
+
+        public BaseRepository(DatabaseContext context)
+        {
+            this.context = context;
+            entities = context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> GetAll() =>
+            entities.AsEnumerable();
+
+        public virtual async Task<List<T>> GetAllAsync() =>
+            await entities.ToListAsync();
+
+        public virtual T Get(long id) => 
+            entities.SingleOrDefault(s => s.Id == id);
+
+        public virtual async Task<T> GetAsync(long id) =>
+            await entities.SingleOrDefaultAsync(s => s.Id == id);
+
+        public virtual T Create(T entity)
+        {
+            var result = entities.Add(entity);
+            context.SaveChanges();
+            return result.Entity;
+        }
+
+        public virtual async Task<T> CreateAsync(T entity)
+        {
+            var result = await entities.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public virtual T Update(T entity)
+        {
+            var result = context.Update(entity);
+            context.SaveChanges();
+            return result.Entity;
+        }
+
+        public virtual async Task<T> UpdateAsync(T entity)
+        {
+            var result = context.Update(entity);
+            await context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            entities.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public virtual async void DeleteAsync(T entity)
+        {
+            entities.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
