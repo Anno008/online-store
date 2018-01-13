f7ad0df0b588d44eec084086fa052c7c6b6b1c11
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-13 21:26:35 +0100

    Simplified brand repository.

--- backend/Repositories/BrandRepository.cs
+++ backend/Repositories/BrandRepository.cs
@@ -1,40 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Backend.Models;
+﻿using Backend.Models;
 
 namespace Backend.Repositories
 {
-    public class BrandRepository
+    public class BrandRepository : BaseRepository<Brand>
     {
-        private readonly DatabaseContext databaseContext;
-
-        public BrandRepository(DatabaseContext databaseContext) =>
-            this.databaseContext = databaseContext;
-
-        public List<Brand> Get() =>
-            databaseContext.Brands.ToList();
-
-        public Brand Get(int id) =>
-            databaseContext.Brands.Find(id);
-
-        public Brand Create(Brand brand)
-        {
-            var result = databaseContext.Brands.Add(brand);
-            databaseContext.SaveChanges();
-            return result.Entity;
-        }
-
-        public Brand Update(Brand brand)
-        {
-            var updated = databaseContext.Brands.Update(brand);
-            databaseContext.SaveChanges();
-            return updated.Entity;
-        }
-
-        public void Delete(int id)
-        {
-            databaseContext.Brands.Remove(Get(id));
-            databaseContext.SaveChanges();
-        }
+        public BrandRepository(DatabaseContext databaseContext) : base(databaseContext) { }
     }
 }
