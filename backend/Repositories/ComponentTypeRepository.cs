8a31dff58e9508d62e9a71fec1c624d874cda52a
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-13 21:26:50 +0100

    Simplified component type repository.

--- backend/Repositories/ComponentTypeRepository.cs
+++ backend/Repositories/ComponentTypeRepository.cs
@@ -1,40 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Backend.Models;
+﻿using Backend.Models;
 
 namespace Backend.Repositories
 {
-    public class ComponentTypeRepository
+    public class ComponentTypeRepository : BaseRepository<ComponentType>
     {
-        private readonly DatabaseContext databaseContext;
-
-        public ComponentTypeRepository(DatabaseContext databaseContext) =>
-            this.databaseContext = databaseContext;
-
-        public List<ComponentType> Get() =>
-            databaseContext.ComponentTypes.ToList();
-
-        public ComponentType Get(int id) =>
-            databaseContext.ComponentTypes.Find(id);
-
-        public ComponentType Create(ComponentType type)
-        {
-            var result = databaseContext.ComponentTypes.Add(type);
-            databaseContext.SaveChanges();
-            return result.Entity;
-        }
-
-        public ComponentType Update(ComponentType type)
-        {
-            var updated = databaseContext.ComponentTypes.Update(type);
-            databaseContext.SaveChanges();
-            return updated.Entity;
-        }
-
-        public void Delete(int id)
-        {
-            databaseContext.ComponentTypes.Remove(Get(id));
-            databaseContext.SaveChanges();
-        }
+        public ComponentTypeRepository(DatabaseContext databaseContext) : base(databaseContext){ }
     }
 }
