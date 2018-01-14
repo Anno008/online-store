57b816de466bb576f0f8c99efe595f280f47fce1
root <root@NEO-PC.localdomain>
2018-01-14 18:42:49 +0100

    Deleted the duplicated lowercase folder

--- backend/Models/Brand.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Backend.Models
-{
-    public class Brand : BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
--- backend/Repositories/ComponentTypeRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Backend.Models;
-
-namespace Backend.Repositories
-{
-    public class ComponentTypeRepository : BaseRepository<ComponentType>
-    {
-        public ComponentTypeRepository(DatabaseContext databaseContext) : base(databaseContext){ }
-    }
-}
