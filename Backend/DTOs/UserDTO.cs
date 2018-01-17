0abc8a4fffe50937b87321f9c4ba858eb4829bd6
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-17 20:52:49 +0100

    Implemented Validate method on the UserDTO

--- Backend/DTOs/UserDTO.cs
+++ Backend/DTOs/UserDTO.cs
@@ -1,8 +1,17 @@
-﻿namespace Backend.DTOs
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.DTOs
 {
-    public class UserDTO
+    public class UserDTO : IValidatableObject
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Username == null || Password == null)
+                yield return new ValidationResult("Must provide username/password");
+        }
     }
 }
