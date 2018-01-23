994825f9ce56380c3050af2f9342836efa0c5e15
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-23 21:24:23 +0100

    Created Refresh token model

---
+++ Backend/Backend.WebApi/Models/RefreshToken.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.WebApi.Models
+{
+    public class RefreshToken : BaseEntity
+    {
+        public string TokenId { get; set; }
+        public string Token { get; set; }
+    }
+}
