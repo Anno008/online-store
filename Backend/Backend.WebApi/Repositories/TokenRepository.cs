54166ab5615231e78656e55e3fee93b62ff7fbbc
Aleksa Jovicic <aleksa.j@outlook.com>
2018-01-24 21:58:04 +0100

    Created token repository

---
+++ Backend/Backend.WebApi/Repositories/TokenRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Backend.WebApi.Models;
+
+namespace Backend.WebApi.Repositories
+{
+    public class TokenRepository : BaseRepository<RefreshToken>
+    {
+        public TokenRepository(DatabaseContext context) : base(context) { }
+
+        public bool TokenExists(string tokenId) =>
+           databaseContext.Tokens.Any(token => token.TokenId == tokenId);
+    }
+}
