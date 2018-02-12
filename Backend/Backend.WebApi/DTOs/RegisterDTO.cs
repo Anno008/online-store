33195dc7bf9bba9fa58d416b24d0a9905a57091b
Aleksa Jovicic <aleksa.j@outlook.com>
2018-02-12 20:35:47 +0100

    Updated the DTOs

---
+++ Backend/Backend.WebApi/DTOs/RegisterDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.WebApi.DTOs
+{
+    public class RegisterDTO : IValidatableObject
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ClientId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Username == null || Password == null)
+                yield return new ValidationResult("Must provide username/password");
+            if (ClientId == null)
+               yield return new ValidationResult("ClientId required");
+        }
+    }
+}
