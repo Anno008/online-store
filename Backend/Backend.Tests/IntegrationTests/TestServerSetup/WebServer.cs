c6666dfd00df9dfaf8d7f9170a052870f55d85d6
Aleksa Jovicic <aleksa.j@outlook.com>
2018-02-13 21:18:02 +0100

    Created WebServer that is going to be used for integration tests

---
+++ Backend/Backend.Tests/IntegrationTests/TestServerSetup/WebServer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+using Backend.WebApi.Models;
+
+namespace Backend.Tests.IntegrationTests.TestServerSetup
+{
+    public class WebServer
+    {
+        protected readonly HttpClient client;
+
+        // Initializing our TestServer
+        public WebServer()
+        {
+            // Arrange
+            var server = new TestServer(new WebHostBuilder()
+                .ConfigureServices(services => services.AddSingleton(TestData()))
+                .UseStartup<TestStartup>());
+
+            client = server.CreateClient();
+        }
+
+        /// <summary>
+        /// This is the initial data that will be
+        /// used to populate our in memory database
+        /// </summary>
+        /// <returns></returns>
+        private TestDataSet TestData()
+        {
+            var testData = new TestDataSet
+            {
+                Users = new List<User>()
+            };
+
+            // Users
+            testData.Users.Add(new User { Username = "User", Password = "User", Role = Role.User });
+            
+
+            return testData;
+        }
+
+    }
+}
