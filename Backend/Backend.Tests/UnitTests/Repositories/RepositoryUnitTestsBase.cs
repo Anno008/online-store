6a0eea49988ada45f20c4d12259dcae57dd49c87
Aleksa Jovicic <aleksa.j@outlook.com>
2018-02-21 18:52:43 +0100

    Created a repository base class to handle the common setup and teardown steps at one place witout repetition

---
+++ Backend/Backend.Tests/UnitTests/Repositories/RepositoryUnitTestsBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Backend.Tests.UnitTests.Helpers;
+using Backend.WebApi.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Tests.UnitTests.Repositories
+{
+    public abstract class RepositoryUnitTestsBase : IDisposable
+    {
+        protected readonly DatabaseContext dbContext;
+
+        public RepositoryUnitTestsBase()
+        {
+            // Setup
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            dbContext = new DatabaseContext(options);
+            dbContext.ResetValueGenerators();
+        }
+
+        public void Dispose() =>
+            dbContext.Dispose();
+    }
+}
