619286e0559f42e69dd07c885db5e5c640c11b52
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-01 20:02:20 +0100

    Created component type controller

---
+++ Backend/Backend.WebApi/Controllers/ComponentTypesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Backend.WebApi.Models;
+using Backend.WebApi.Repositories;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ComponentTypesController : Controller
+    {
+        private readonly ComponentTypeRepository componentTypeRepository;
+
+        public ComponentTypesController(ComponentTypeRepository componentTypeRepository) =>
+            this.componentTypeRepository = componentTypeRepository;
+
+        [HttpGet]
+        public Task<List<ComponentType>> Get() =>
+            componentTypeRepository.GetAllAsync();
+
+        [HttpGet("{id}")]
+        public Task<ComponentType> Get(int id) =>
+            componentTypeRepository.GetAsync(id);
+
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = "User")]
+        public Task<ComponentType> Post([FromBody]ComponentType ComponentType) =>
+            componentTypeRepository.CreateAsync(ComponentType);
+
+        [HttpPut("{id}")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = "User")]
+        public Task<ComponentType> Put(int id, [FromBody]ComponentType ComponentType) =>
+            componentTypeRepository.UpdateAsync(ComponentType);
+
+        [HttpDelete("{id}")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = "User")]
+        public void Delete(int id) =>
+            componentTypeRepository.DeleteAsync(id);
+    }
+}
