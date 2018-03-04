d5c6fb9d865377ea394a4818ebd5de6bfced34a2
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-04 14:51:31 +0100

    Created Component request and response DTO

---
+++ Backend/Backend.WebApi/DTOs/ComponentsRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.WebApi.DTOs
+{
+    public class ComponentsRequestDTO
+    {
+        public string ComponentName { get; set; }
+        public int[] BrandIds { get; set; }
+        public int ComponentTypeId { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public OrderComponentsBy OrderBy { get; set; }
+    }
+}
---
+++ Backend/Backend.WebApi/DTOs/ComponentsResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Backend.WebApi.Models;
+
+namespace Backend.WebApi.DTOs
+{
+    public class ComponentsResponseDTO
+    {
+        public ComponentsResponseDTO((List<Component> components, int totalPages, int totalItems, int itemsOnPage, int currentPage) pageableParams)
+        {
+            Components = pageableParams.components;
+            TotalItems = pageableParams.totalItems;
+            Pages = pageableParams.totalPages;
+            ItemsOnPage = pageableParams.itemsOnPage;
+            CurrentPage = pageableParams.currentPage;
+        }
+     
+        public List<Component> Components { get; set; }
+        public int TotalItems { get; set; }
+        public int Pages { get; set; }
+        public int ItemsOnPage { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
