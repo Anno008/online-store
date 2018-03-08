083b3f9d0201dc68d9fb3b97dee78cece0528b96
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-08 18:47:39 +0100

    Updated RepositoryUnitTestsBase, fixed the issue with randomly occuring unit tests failure due to not properly re generating a new database context instance after each unit test. That was handled by instancing a new ServiceProvider and providing it to the db contextoptions builder

--- Backend/Backend.Tests/UnitTests/Repositories/RepositoryUnitTestsBase.cs
+++ Backend/Backend.Tests/UnitTests/Repositories/RepositoryUnitTestsBase.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using Backend.Tests.UnitTests.Helpers;
 using Backend.WebApi.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Backend.Tests.UnitTests.Repositories
 {
@@ -9,20 +9,31 @@ namespace Backend.Tests.UnitTests.Repositories
     {
         protected readonly DatabaseContext dbContext;
 
-        public RepositoryUnitTestsBase()
+        protected RepositoryUnitTestsBase()
         {
             // Setup
-            var options = new DbContextOptionsBuilder<DatabaseContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
-
-            dbContext = new DatabaseContext(options);
-            // a work around to reset the auto generation after each test,
-            // far from ideal since it occasionally causes the tests to fail
-            dbContext.ResetValueGenerators();
+            dbContext = new DatabaseContext(CreateNewContextOptions());
         }
 
         public void Dispose() =>
             dbContext.Dispose();
+
+        private DbContextOptions<DatabaseContext> CreateNewContextOptions()
+        {
+            // Create a fresh service provider, and therefore a fresh 
+            // InMemory database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance telling the context to use an
+            // InMemory database and the new service provider.
+            return new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .UseInternalServiceProvider(serviceProvider)
+                .Options;
+        }
     }
+
+    
 }
