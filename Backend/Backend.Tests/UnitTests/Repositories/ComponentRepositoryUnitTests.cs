2be42b7693878d5c93b4614a5480e3e2fb6ceafc
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-08 20:53:29 +0100

    Updated ComponentRepositoryUnitTests tested the other crud operations

--- Backend/Backend.Tests/UnitTests/Repositories/ComponentRepositoryUnitTests.cs
+++ Backend/Backend.Tests/UnitTests/Repositories/ComponentRepositoryUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using Backend.WebApi.DTOs;
+﻿using System.Linq;
+using Backend.WebApi.DTOs;
 using Backend.WebApi.Models;
 using Backend.WebApi.Repositories;
 using Xunit;
@@ -157,6 +158,229 @@ namespace Backend.Tests.UnitTests.Repositories
             Assert.Equal(3, totalPages);
             Assert.Equal(3, itemsOnPage);
             Assert.Equal(1, currentPage);
+        }
+
+        [Fact]
+        public void CreateComponent_WithUnexistingBrandAndComponentType_ReturnNull()
+        {
+            var unexistingBrand = new Brand { Id = 5, Name = "Abc" };
+            var componentType = new ComponentType { Id = 5, Name = "Abc" };
+
+            var component = new Component
+            {
+                Name = "Test",
+                Brand = unexistingBrand,
+                Type = componentType,
+                Price = 123,
+            };
+
+            var result = componentRepository.Create(component);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void CreateComponentAsync_WithUnexistingBrandAndComponentType_ReturnNull()
+        {
+            var unexistingBrand = new Brand { Id = 5, Name = "Abc" };
+            var componentType = new ComponentType { Id = 5, Name = "Abc" };
+
+            var component = new Component
+            {
+                Name = "Test",
+                Brand = unexistingBrand,
+                Type = componentType,
+                Price = 123,
+            };
+
+            var result = await componentRepository.CreateAsync(component);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void CreateComponent_WithValidBrandAndComponentType_ReturnCreatedComponent()
+        {
+            var brand = new Brand { Id = 1, Name = "Intel" };
+            var type = new ComponentType { Id = 1, Name = "CPU" };
+
+            var component = new Component
+            {
+                Name = "Test",
+                Brand = brand,
+                Type = type,
+                Price = 123,
+            };
+
+            var result = componentRepository.Create(component);
+            Assert.NotNull(result);
+            Assert.Equal(component.Name, result.Name);
+            Assert.Equal(component.Brand.Name, result.Brand.Name);
+            Assert.Equal(component.Type.Name, result.Type.Name);
+            Assert.Equal(9, dbContext.Components.Count());
+        }
+
+        [Fact]
+        public async void CreateComponentAsync_WithValidBrandAndComponentType_ReturnCreatedComponent()
+        {
+            var brand = new Brand { Id = 1, Name = "Intel" };
+            var type = new ComponentType { Id = 1, Name = "CPU" };
+
+            var component = new Component
+            {
+                Name = "Test",
+                Brand = brand,
+                Type = type,
+                Price = 123,
+            };
+
+            var result = await componentRepository.CreateAsync(component);
+            Assert.NotNull(result);
+            Assert.Equal(component.Name, result.Name);
+            Assert.Equal(component.Brand.Name, result.Brand.Name);
+            Assert.Equal(component.Type.Name, result.Type.Name);
+            Assert.Equal(9, dbContext.Components.Count());
+        }
+
+        [Fact]
+        public void UpdateComponent_ComponentDoesntExist_ReturnNull()
+        {
+            var unexistingBrand = new Brand { Id = 5, Name = "Abc" };
+            var componentType = new ComponentType { Id = 5, Name = "Abc" };
+
+            var component = new Component
+            {
+                Id = 111,
+                Name = "Test",
+                Brand = unexistingBrand,
+                Type = componentType,
+                Price = 123,
+            };
+
+            var result = componentRepository.Update(component);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void UpdateComponent_WithUnexistingBrandAndComponentType_ReturnNull()
+        {
+            var unexistingBrand = new Brand { Id = 5, Name = "Abc" };
+            var componentType = new ComponentType { Id = 5, Name = "Abc" };
+
+            var component = new Component
+            {
+                Name = "Test",
+                Brand = unexistingBrand,
+                Type = componentType,
+                Price = 123,
+            };
+
+            var result = componentRepository.Create(component);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void UpdateComponentAsync_WithUnexistingBrandAndComponentType_ReturnNull()
+        {
+            var unexistingBrand = new Brand { Id = 5, Name = "Abc" };
+            var componentType = new ComponentType { Id = 5, Name = "Abc" };
+
+            var component = new Component
+            {
+                Name = "Test",
+                Brand = unexistingBrand,
+                Type = componentType,
+                Price = 123,
+            };
+
+            var result = await componentRepository.CreateAsync(component);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void UpdateComponent_WithValidBrandAndComponentType_ReturnCreatedComponent()
+        {
+            var brand1 = new Brand { Id = 1, Name = "Intel" };
+            var brand2 = new Brand { Id = 2, Name = "AMD" };
+
+            var type1 = new ComponentType { Id = 1, Name = "GPU" };
+            var type2 = new ComponentType { Id = 2, Name = "CPU" };
+
+            var component = new Component
+            {
+                Id = 1,
+                Brand = brand2,
+                Type = type2,
+                Name = "AMD FX 6600",
+                Price = 150
+            };
+
+            var result = componentRepository.Update(component);
+            Assert.NotNull(result);
+            Assert.Equal(component.Name, result.Name);
+            Assert.Equal(component.Brand.Name, result.Brand.Name);
+            Assert.Equal(component.Type.Name, result.Type.Name);
+            Assert.Equal(8, dbContext.Components.Count());
+        }
+
+        [Fact]
+        public async void UpdateComponentAsync_WithValidBrandAndComponentType_ReturnCreatedComponent()
+        {
+            var brand1 = new Brand { Id = 1, Name = "Intel" };
+            var brand2 = new Brand { Id = 2, Name = "AMD" };
+
+            var type1 = new ComponentType { Id = 1, Name = "GPU" };
+            var type2 = new ComponentType { Id = 2, Name = "CPU" };
+
+            var component = new Component
+            {
+                Id = 1,
+                Brand = brand2,
+                Type = type2,
+                Name = "AMD FX 6600",
+                Price = 150
+            };
+
+            var result = await componentRepository.UpdateAsync(component);
+            Assert.NotNull(result);
+            Assert.Equal(component.Name, result.Name);
+            Assert.Equal(component.Brand.Name, result.Brand.Name);
+            Assert.Equal(component.Type.Name, result.Type.Name);
+            Assert.Equal(8, dbContext.Components.Count());
+        }
+
+        [Fact]
+        public void Delete_ShouldDeleteTheComponentFromTheDatabase()
+        {
+            var componentId = 1;
+
+            componentRepository.Delete(componentId);
+            Assert.Equal(7, dbContext.Components.Count());
+        }
+
+        [Fact]
+        public void DeleteAsync_ShouldDeleteTheComponentFromTheDatabase()
+        {
+            var componentId = 1;
+
+            componentRepository.DeleteAsync(componentId);
+            Assert.Equal(7, dbContext.Components.Count());
+        }
+
+        [Fact]
+        public void Delete_AttemptToDeleteAnEntityThatDoesntExist_ShouldntChangeTheTotalCount()
+        {
+            var componentId = 22;
+
+            componentRepository.Delete(componentId);
+            Assert.Equal(8, dbContext.Components.Count());
+        }
+
+        [Fact]
+        public void DeleteAsync_AttemptToDeleteAnEntityThatDoesntExist_ShouldntChangeTheTotalCount()
+        {
+            var componentId = 22;
+
+            componentRepository.DeleteAsync(componentId);
+            Assert.Equal(8, dbContext.Components.Count());
         }
     }
 }
