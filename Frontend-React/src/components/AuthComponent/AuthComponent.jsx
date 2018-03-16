ea414d5a3a6175bb67230531be2e1297279a2315
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-16 21:04:29 +0100

    Created placeholder shopping and auth components

---
+++ Frontend-React/src/components/AuthComponent/AuthComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AuthComponent = () => (
+  <React.Fragment>
+    <div className="filter">Login/Register</div>
+    <div className="main">Login / Register forms</div>
+  </React.Fragment>
+);
+
+export default AuthComponent;
---
+++ Frontend-React/src/components/ShoppingComponent/ShoppingComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ShoppingComponent = () => (
+  <React.Fragment>
+    <div className="filter">Filter</div>
+    <div className="main">Main</div>
+  </React.Fragment>
+);
+
+export default ShoppingComponent;
