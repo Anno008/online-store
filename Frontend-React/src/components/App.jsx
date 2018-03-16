d6f50ce0eb8cf760b5dc0586d26ad66d79d1952f
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-16 21:07:48 +0100

    Updated App.jsx, the main component

--- Frontend-React/src/components/App.jsx
+++ Frontend-React/src/components/App.jsx
@@ -1,38 +1,20 @@
 import React from "react";
-import ReactLogo from "assets/react.svg";
-import ReduxLogo from "assets/redux.svg";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import "css/app.css";
+import ShoppingComponent from "./ShoppingComponent/ShoppingComponent";
+import AuthComponent from "./AuthComponent/AuthComponent";
 
 const App = () => (
-    <div className="app">
-        <header className="app-header">
-            <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-                <img className="app-logo" src={ReactLogo} alt="react-logo" />
-            </a>
-            <a href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-                <img className="app-logo-rev" src={ReduxLogo} alt="redux-logo" />
-            </a>
-            <h1 className="app-title">Welcome to React & Redux!</h1>
-            <Router>
-                <div>
-                    <Route exact path="/" component={FirstComponent} />
-                    <Route path="/auth" component={SecondComponent} />
-                </div>
-            </Router>
-        </header>
-    </div>);
-
-const FirstComponent = () => (
-    <div>
-        <h1>First</h1>
-    </div>
-);
-
-const SecondComponent = () => (
-    <div>
-        <h1>Second</h1>
-    </div>
+  <div className="app">
+    <div className="navBar" />
+    <Router>
+      <React.Fragment>
+        <Route exact path="/" component={ShoppingComponent} />
+        <Route path="/auth" component={AuthComponent} />
+      </React.Fragment>
+    </Router>
+    <div className="footer" />
+  </div>
 );
 
 export default App;
