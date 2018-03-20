1f3cfd0a66ede3074256897a476df29d73b7ff05
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-20 22:27:12 +0100

    Updated api wrapper, with a slightly improved handler for retreiving access/refesh token from local storage. Needs improvement

--- Frontend-React/src/api/ApiWrapper.js
+++ Frontend-React/src/api/ApiWrapper.js
@@ -2,47 +2,59 @@ import { apiUrl, redirectUri } from "../constants";
 import jwt_decode from "jwt-decode";
 
 const apiCall = async config => {
-  console.log(config);
-  console.log(apiUrl);
-
   const request = {
     data: config.body,
-    headers: config.needsAuth ? await createHeader(config.headers)
-      : config.headers,
+    headers: config.needsAuth ? await createHeader() : {},
     method: config.method || "GET",
     url: config.url
   };
 
   return await fetch(request.url, {
     method: request.method,
-    body: request.method == "GET" ? undefined : request.data,
+    body: request.method == "GET" ? undefined : JSON.stringify(request.data),
     headers: request.headers
   })
-    .then(response => {
-      response;
-      console.log(response);
-    })
+    .then(response => response.json().then(content => content))
     .catch(error => {
-      console.log(error);
       throw new Error(error);
     });
 };
 
 const createHeader = async () => {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
   let token = localStorage.getItem("accessToken");
+
   if (token) {
     const decoded = jwt_decode(token);
+
+    // Access token has expired
     if (decoded.exp * 1000 < Date.now()) {
-    //   token = await getRefreshToken(localStorage.getItem(refreshToken));
+      const refreshToken = localStorage.getItem("refreshToken");
+      if (refreshToken) {
+        const refreshedAccessToken = await fetch(`${apiUrl}/Auth/auth`, {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify({ grantType: "refresh_token" })
+        })
+          .then(response => response.json().then(res => res.accessToken))
+          .catch(error => {
+            throw new Error("Failed to refresh access token");
+          });
+        token = refreshedAccessToken;
+      } else {
+        location.href = redirectUri;
+      }
     }
   } else {
     location.href = redirectUri;
-    return;
   }
 
-  return token
-    ? { authorization: `bearer ${token}`, ...(headers || {}) }
-    : headers || {};
+  if (token) {
+    headers.append("authorization", `bearer ${token}`);
+  }
+
+  return headers;
 };
 
 export default apiCall;
