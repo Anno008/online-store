b6c544f531349b7b6abe614c6325c53cff181cd3
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-21 19:13:40 +0100

    Created initial brand actions

---
+++ Frontend-React/src/actions/BrandActions.js
@@ -0,0 +1,28 @@
+import actions from "./Actions";
+import apiCall from "../api/ApiWrapper";
+import { apiUrl } from "../constants";
+
+const getData = () => ({ type: actions.FETCHING_BRANDS });
+
+const getDataSuccess = data => ({
+  type: actions.FETCHING_BRANDS_SUCCESS,
+  data
+});
+
+const getDataFailure = error => ({
+  type: actions.FETCHING_BRANDS_FAILURE,
+  error
+});
+
+export const fetchBrands = () => dispatch => {
+  dispatch(getData());
+  const config = {
+    method: "GET",
+    needsAuth: true,
+    url: `${apiUrl}/brands`
+  };
+
+  return apiCall(config)
+    .then(result => dispatch(getDataSuccess(result)))
+    .catch(error => dispatch(getDataFailure(error)));
+};
