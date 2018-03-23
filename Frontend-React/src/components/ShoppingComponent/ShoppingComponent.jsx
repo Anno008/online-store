b42a36258e67bf451b1c2aba8cd2d17004216736
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-23 22:21:13 +0100

    Updated the shopping component. Now it calls the initial fetch brands

--- Frontend-React/src/components/ShoppingComponent/ShoppingComponent.jsx
+++ Frontend-React/src/components/ShoppingComponent/ShoppingComponent.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import FilterComponent from "./FilterComponent";
+import { fetchBrands } from "../../actions/BrandActions";
+import apiCall from "../../api/ApiWrapper";
 
-const ShoppingComponent = () => (
-  <React.Fragment>
-    <div className="filter">Filter</div>
-    <div className="main">Main</div>
-  </React.Fragment>
-);
+class ShoppingComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    props.initializeBrands();
+  }
 
-const mapStateToProps = (state) => ({
+  render() {
+    return (
+      <React.Fragment>
+        <div className="filter">
+          <FilterComponent />
+        </div>
+        <div className="main">Main</div>
+      </React.Fragment>
+    );
+  }
+}
 
-});
+const mapStateToProps = state => ({});
 
-const mapDispatchToProps = (dispatch) => ({
-
+const mapDispatchToProps = dispatch => ({
+  initializeBrands: () => dispatch(fetchBrands())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShoppingComponent);
