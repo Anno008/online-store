ec549997bc1a34b3210db30d74acfd7a82f20969
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-23 19:57:49 +0100

    Created AuthActions.js. Handles the login and register actions

---
+++ Frontend-React/src/actions/AuthActions.js
@@ -0,0 +1,72 @@
+import actions from "./Actions";
+import apiCall from "../api/ApiWrapper";
+import { apiUrl, clientId } from "../constants";
+import jwt_decode from "jwt-decode";
+
+const authInProgress = () => ({ type: actions.AUTH_IN_PROGRESS });
+
+const authSuccessful = (username, roles) => ({
+  type: actions.AUTH_SUCCESS,
+  data: { username, roles }
+});
+
+const authFailed = error => ({
+  type: actions.AUTH_FAILURE,
+  error: error
+});
+
+export const login = (username, password, rememberMe) => dispatch => {
+  dispatch(authInProgress());
+  const config = {
+    method: "POST",
+    data: {
+      username: username,
+      password: password,
+      clientId: clientId,
+      rememberMe: rememberMe,
+      grantType: "password"
+    },
+    url: `${apiUrl}/auth/auth`
+  };
+
+  return apiCall(config)
+    .then(result => {
+      localStorage.setItem("accessToken", result.accessToken);
+      localStorage.setItem("refreshToken", result.refreshToken);
+
+      const decodedToken = jwt_decode(result.accessToken);
+      return dispatch(
+        authSuccessful(decodedToken.user_name, decodedToken.roles)
+      );
+    })
+    .catch(error => {
+      return dispatch(authFailed(error.message));
+    });
+};
+
+export const register = (username, password) => dispatch => {
+  dispatch(authInProgress());
+
+  const config = {
+    method: "POST",
+    needsAuth: false,
+    data: {
+      username: username,
+      password: password,
+      clientId: clientId
+    },
+    url: `${apiUrl}/auth/register`
+  };
+
+  return apiCall(config)
+    .then(token => {
+      localStorage.setItem("accessToken", token.accessToken);
+      const decodedToken = jwt_decode(token.accessToken);
+      return dispatch(
+        authSuccessful(decodedToken.user_name, decodedToken.roles)
+      );
+    })
+    .catch(error => {
+      dispatch(authFailed(error.message));
+    });
+};
