cb11590a2aeb1850b4b6c096a9792011caf3ff16
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-23 21:58:54 +0100

    Updated the root reducer with the auth reducer

--- Frontend-React/src/reducers/index.js
+++ Frontend-React/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { BrandsReducer } from "./BrandsReducer";
+import { AuthReducer } from "./AuthReducer";
 
 const reducers = combineReducers({
-   brandsState: BrandsReducer
+   brandsState: BrandsReducer,
+   authState: AuthReducer
 });
 
 export default reducers;
