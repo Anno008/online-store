b49e7eace08918b804247f9b2aa23c733603d2cc
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-24 21:47:24 +0100

    Updated App.jsx component. Once the component is rendered it will check if the user is looged in or not

--- Frontend-React/src/components/App.jsx
+++ Frontend-React/src/components/App.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+import { connect } from "react-redux";
 import "css/app.css";
 import ShoppingComponent from "./ShoppingComponent/ShoppingComponent";
 import AuthComponent from "./AuthComponent/AuthComponent";
+import NavComponent from "./NavComponent/NavComponent";
+import { checkForUser } from "../actions/AuthActions";
 
-const App = () => (
-  <div className="app">
-    <div className="navBar" />
-    <Router>
-      <React.Fragment>
-        <Route exact path="/" component={ShoppingComponent} />
-        <Route path="/auth" component={AuthComponent} />
-      </React.Fragment>
-    </Router>
-    <div className="footer" />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    props.checkIfTheUserWasLoggedIn();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="app">
+        <NavComponent />
+        <Router>
+          <React.Fragment>
+            <Route exact path="/" component={ShoppingComponent} />
+            <Route path="/auth" component={AuthComponent} />
+          </React.Fragment>
+        </Router>
+        <div className="footer" />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({
+  checkIfTheUserWasLoggedIn: () => dispatch(checkForUser())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
