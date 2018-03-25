7703d9bdd27385f164df2f418a93d2d08ca22b7e
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-25 17:46:33 +0200

    Updated the auth component. Now it handles if the user wants to login or register

--- Frontend-React/src/components/AuthComponent/AuthComponent.jsx
+++ Frontend-React/src/components/AuthComponent/AuthComponent.jsx
@@ -1,10 +1,63 @@
 import React from "react";
+import { login, register } from "../../actions/AuthActions";
+import { connect } from "react-redux";
+import LoginComponent from "./LoginComponent";
+import RegisterComponent from "./RegisterComponent";
+import "components/css/label.css";
 
-const AuthComponent = () => (
-  <React.Fragment>
-    <div className="filter">Login/Register</div>
-    <div className="main">Login / Register forms</div>
-  </React.Fragment>
-);
+class AuthComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    if (props.userState.data != undefined) {
+      location.href = "/";
+    }
+  }
 
-export default AuthComponent;
+  componentWillMount() {
+    this.setState({
+      login: true
+    });
+  }
+
+  handleLinkClick(e, login) {
+    this.setState({
+      login: login
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="filter">
+          <p>
+            {this.props.userState.error ? this.props.userState.error : null}
+          </p>
+        </div>
+        <div className="main">
+          {this.state.login ? <LoginComponent /> : <RegisterComponent />}
+          {this.state.login ? 
+            <label
+              className="simpleLabel"
+              onClick={e => this.handleLinkClick(e, false)}>
+              Don't have an account
+            </label>
+              : 
+            <label
+              className="simpleLabel"
+              onClick={e => this.handleLinkClick(e, true)}>
+              Already have an account? Sign in
+            </label>
+          }
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  userState: state.userState
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthComponent);
