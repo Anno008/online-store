21fc06d36d697a6bdb6d8e9401390f465e43224c
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-25 18:06:11 +0200

    Created BrandControllerTest

---
+++ Backend/Backend.Tests/IntegrationTests/ControllerTests/BrandsControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using Backend.Tests.IntegrationTests.TestServerSetup;
+using Backend.WebApi.Controllers;
+using Backend.WebApi.Models;
+using Backend.WebApi.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Backend.Tests.IntegrationTests.ControllerTests
+{
+    public class BrandsControllerTests : WebServer
+    {
+        private readonly BrandRepository brandRepository;
+        
+        public BrandsControllerTests()
+        {
+            // Setup
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var dbContext = new DatabaseContext(options);
+            brandRepository = new BrandRepository(dbContext);
+            brandRepository.Create(new Brand { Name = "Test" });
+        }
+
+        [Fact]
+        public async Task GetAllBrands_ShouldReturnList()
+        {
+            // Act
+            var response = await client.GetAsync("api/brands");
+
+            var t = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var controller = new BrandsController(brandRepository);
+            var result = await controller.Get();
+
+            Assert.True(result.Count > 0);
+        }
+
+    }
+}
