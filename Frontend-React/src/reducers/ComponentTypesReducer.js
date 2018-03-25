0dd54d01d0c460f8e9d2e027780df4c329ea6672
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-25 21:53:21 +0200

    Created ComponentTypeReducer.js

---
+++ Frontend-React/src/reducers/ComponentTypesReducer.js
@@ -0,0 +1,14 @@
+import actions from "../actions/Actions";
+
+export const BrandsReducer = (state, action) => {
+  switch (action.type) {
+    case actions.FETCHING_COMPONENT_TYPES:
+      return { ...state, data: [], isFetching: true };
+    case actions.FETCHING_COMPONENT_TYPES_SUCCESS:
+      return { ...state, data: action.data, error: false, isFetching: false };
+    case actions.FETCHING_COMPONENT_TYPES_FAILURE:
+      return { ...state, data: [], error: action.error, isFetching: false };
+    default:
+      return { ...state };
+  }
+};
