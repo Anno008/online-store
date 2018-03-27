27a5305482031ff17c97a8b466cef8b23d4f4858
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-27 21:37:03 +0200

    Added the ComponentTypesReducer to the root reducer, aka connected it to the store provider

--- Frontend-React/src/reducers/index.js
+++ Frontend-React/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { BrandsReducer } from "./BrandsReducer";
 import { AuthReducer } from "./AuthReducer";
+import { ComponentTypesReducer } from "./ComponentTypesReducer";
 
 const reducers = combineReducers({
    brandsState: BrandsReducer,
-   userState: AuthReducer
+   userState: AuthReducer,
+   componentTypesState: ComponentTypesReducer
 });
 
 export default reducers;
