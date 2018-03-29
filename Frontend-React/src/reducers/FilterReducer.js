28d9504fc41e2360174dc931685865a268f0893d
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-29 19:16:26 +0200

    Created filter reducer

---
+++ Frontend-React/src/reducers/FilterReducer.js
@@ -0,0 +1,16 @@
+import actions from "../actions/Actions";
+
+const initialComponentFilter = { componentName: "", brandIds: [], componentTypeId: 0 };
+
+export const FilterReducer = (state = initialComponentFilter, action) => {
+  switch (action.type) {
+    case actions.SEARCH_BAR_TEXT_CHANGED:
+      return { ...state, componentName: action.data };
+    case actions.BRAND_SELECTION_CHANGED:
+      return { ...state, componentName: action.data };
+    case actions.COMPONENT_TYPE_SELECTION_CHANGED:
+      return { ...state, componentName: action.data };
+    default:
+      return { ...state };
+  }
+};
