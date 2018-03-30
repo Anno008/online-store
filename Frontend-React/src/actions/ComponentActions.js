9167f93a437a1e2af6eec557edeb4584f342f065
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-30 21:23:34 +0200

    Created ComponentActions.js

---
+++ Frontend-React/src/actions/ComponentActions.js
@@ -0,0 +1,29 @@
+import actions from "./Actions";
+
+const fetchingComponents = () => ({ type: actions.FETCHING_COMPONENTS });
+
+const fetchingComponentsSucceeded = data => ({
+  type: actions.FETCHING_COMPONENTS_SUCCESS,
+  data
+});
+
+const fetchingComponentsFailed = error => ({
+  type: actions.FETCHING_COMPONENTS_FAILURE,
+  error
+});
+
+export const fetchComponents = (filter, paging) => dispatch => {
+  dispatch(fetchingComponents());
+  const config = {
+    method: "GET",
+    url: `${apiUrl}/components?componentName=${filter.componentName}&
+        BrandId=${filter.brandId}&
+        componentTypeId=${filter.componentTypeId}
+        page=${paging.page}&
+        pageSize=${paging.pageSize}&
+        orderBy=${0}`
+  };
+  return apiCall(config)
+    .then(result => dispatch(fetchingComponentsSucceeded(result)))
+    .catch(error => dispatch(fetchingComponentsFailed(error.message)));
+};
