4d2a79a095ed66a29a0a7833fba344d37141462f
Aleksa Jovicic <aleksa.j@outlook.com>
2018-03-31 21:23:20 +0200

    Fixed a naming issue with regards to the paging reducer initial paging state

--- Frontend-React/src/reducers/PagingReducer.js
+++ Frontend-React/src/reducers/PagingReducer.js
@@ -2,7 +2,7 @@ import actions from "../actions/Actions";
 
 const initialPaging = { page: 1, pageSize: 10 };
 
-export const PagingReducer = (state = initialComponentFilter, action) => {
+export const PagingReducer = (state = initialPaging, action) => {
   switch (action.type) {
     case actions.PAGE_CHANGED:
       return { ...state, page: action.data };
