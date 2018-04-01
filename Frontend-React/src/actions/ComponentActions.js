bfa2a909254c9b52facd3f9fc0373861c9672d84
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-01 16:45:57 +0200

    Refactored the query string

--- Frontend-React/src/actions/ComponentActions.js
+++ Frontend-React/src/actions/ComponentActions.js
@@ -16,15 +16,20 @@ const fetchingComponentsFailed = error => ({
 
 export const fetchComponents = (filter, paging) => dispatch => {
   dispatch(fetchingComponents());
+
+  const query =
+    `${apiUrl}/components?componentName=${filter.componentName}&` +
+    `brandId=${filter.brandId}&` +
+    `componentTypeId=${filter.componentTypeId}&` +
+    `page=${paging.page}&` +
+    `pageSize=${paging.pageSize}&` +
+    `orderBy=${0}`;
+
   const config = {
     method: "GET",
-    url: `${apiUrl}/components?componentName=${filter.componentName}&
-        BrandId=${filter.brandId}&
-        componentTypeId=${filter.componentTypeId}
-        page=${paging.page}&
-        pageSize=${paging.pageSize}&
-        orderBy=${0}`
+    url: query
   };
+
   return apiCall(config)
     .then(result => dispatch(fetchingComponentsSucceeded(result)))
     .catch(error => dispatch(fetchingComponentsFailed(error.message)));
