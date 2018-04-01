7861880cd7a4a30e4297dd47d5e2d3bebffd4479
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-01 16:54:23 +0200

    Updated brandsFilterComponent. Now dispatching brandSelectionChanged action

--- Frontend-React/src/components/ShoppingComponent/FilterComponents/BrandsFilterComponent.jsx
+++ Frontend-React/src/components/ShoppingComponent/FilterComponents/BrandsFilterComponent.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { brandSelectionChanged } from "actions/FilterActions";
 
 const BrandsFilterComponent = props => (
-    <select>
-      <option>All</option>
+    <select onChange={(e) => props.handleBrandSelectionChanged(e.currentTarget.value)}>
+      <option value="0">All</option>
       {props.brandsState.data
         ? props.brandsState.data.map(item => (
-            <option key={item.id}>{item.name} </option>
+            <option key={item.id} value={item.id}>{item.name}</option>
           ))
         : null}
     </select>
@@ -16,6 +17,8 @@ const mapStateToProps = state => ({
   brandsState: state.brandsState
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  handleBrandSelectionChanged: (brandId) => dispatch(brandSelectionChanged(brandId))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(BrandsFilterComponent);
