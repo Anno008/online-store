ae40262dc3986951072951e15fa2ee619fd066bc
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-01 15:58:01 +0200

    Updated the flter component with a callback function that will get called whenever a filter/sorting component updates the store

--- Frontend-React/src/components/ShoppingComponent/FilterComponent.jsx
+++ Frontend-React/src/components/ShoppingComponent/FilterComponent.jsx
@@ -3,22 +3,32 @@ import { connect } from "react-redux";
 import BrandsFilterComponent from "./FilterComponents/BrandsFilterComponent";
 import ComponentsNameFilterComponent from "./FilterComponents/ComponentsNameFilterComponent";
 import ComponentTypesFilterComponent from "./FilterComponents/ComponentTypesFilterComponent";
+import { fetchComponents } from "actions/ComponentActions";
 
-const FilterComponent = props => (
-  <React.Fragment>
-    {props.brandsState.error ? <p>{props.brandsState.error}</p> :
-    <React.Fragment>
-       <ComponentsNameFilterComponent />
-       <BrandsFilterComponent />
-       <ComponentTypesFilterComponent />
-    </React.Fragment>}
-  </React.Fragment>
-);
+const FilterComponent = props => {
+  const fetchComponents = () => 
+    props.fetchComponents(props.filterState, props.pagingState);  
+
+  return (
+   <React.Fragment >
+     {props.brandsState.error ? <p>{props.brandsState.error}</p> :
+      <React.Fragment>
+        <ComponentsNameFilterComponent fetchComponents={fetchComponents}/>
+        <BrandsFilterComponent fetchComponents={fetchComponents}/>
+        <ComponentTypesFilterComponent fetchComponents={fetchComponents}/>
+      </React.Fragment>}
+   </React.Fragment>
+  );
+};
 
 const mapStateToProps = state => ({
-  brandsState: state.brandsState
+  brandsState: state.brandsState,
+  filterState: state.filterState,
+  pagingState: state.pagingState
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  fetchComponents: (filter, paging) => dispatch(fetchComponents(filter, paging))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(FilterComponent);
