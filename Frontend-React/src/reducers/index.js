5446ccc964d366afafa41b97f8dd5642b8a600f6
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-03 20:23:10 +0200

    Added components reducer to the root reducer

--- Frontend-React/src/reducers/index.js
+++ Frontend-React/src/reducers/index.js
@@ -5,13 +5,15 @@ import { FilterReducer } from "./FilterReducer";
 import { PagingReducer } from "./PagingReducer";
 
 import { ComponentTypesReducer } from "./ComponentTypesReducer";
+import { ComponentsReducer } from "./ComponentsReducer";
 
 const reducers = combineReducers({
   brandsState: BrandsReducer,
   userState: AuthReducer,
   componentTypesState: ComponentTypesReducer,
   filterState: FilterReducer,
-  pagingState: PagingReducer
+  pagingState: PagingReducer,
+  componentsState: ComponentsReducer
 });
 
 export default reducers;
