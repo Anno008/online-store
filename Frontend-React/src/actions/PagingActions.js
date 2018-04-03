a4c776eccc6ecf2eab56968ee3cd88f0c8f75f30
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-03 20:03:21 +0200

    Exported the paging actions

--- Frontend-React/src/actions/PagingActions.js
+++ Frontend-React/src/actions/PagingActions.js
@@ -1,7 +1,7 @@
 import actions from "./Actions";
 
-const pageNumberChanged = page => dispatch =>
+export const pageNumberChanged = page => dispatch =>
   dispatch({ type: actions.PAGE_CHANGED, data: page });
 
-const pageSizeChanged = pageSize => dispatch =>
+export const pageSizeChanged = pageSize => dispatch =>
   dispatch({ type: actions.PAGE_SIZE_CHANGED, data: pageSize });
