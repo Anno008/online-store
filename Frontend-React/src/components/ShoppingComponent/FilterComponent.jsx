7b830d75e8f472f08083781d09d5604635f79fcd
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-07 14:40:18 +0200

    Updated filter component

--- Frontend-React/src/components/ShoppingComponent/FilterComponent.jsx
+++ Frontend-React/src/components/ShoppingComponent/FilterComponent.jsx
@@ -4,16 +4,17 @@ import BrandsFilterComponent from "./FilterComponents/BrandsFilterComponent";
 import ComponentsNameFilterComponent from "./FilterComponents/ComponentsNameFilterComponent";
 import ComponentTypesFilterComponent from "./FilterComponents/ComponentTypesFilterComponent";
 import { fetchComponents } from "actions/ComponentActions";
+import "../css/filter.css";
 
 const FilterComponent = (props) => {
   return (
    <React.Fragment >
      {props.brandsState.error ? <p>{props.brandsState.error}</p> :
-      <React.Fragment>
+      <div className="filterContainer">
         <ComponentsNameFilterComponent/>
         <BrandsFilterComponent/>
         <ComponentTypesFilterComponent/>
-      </React.Fragment>}
+      </div>}
    </React.Fragment>
   );
 };
