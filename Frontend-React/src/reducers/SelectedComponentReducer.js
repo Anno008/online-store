5f5e63e25080ebc1b516d8ccb87c34336c469760
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-17 20:02:53 +0200

    Created selected component reducer

---
+++ Frontend-React/src/reducers/SelectedComponentReducer.js
@@ -0,0 +1,14 @@
+import actions from "../actions/Actions";
+
+export const SelectedComponentReducer = (state, action) => {
+  switch (action.type) {
+    case actions.FETCHING_ONE_COMPONENT:
+      return { ...state, isFetching: true };
+    case actions.FETCHING_ONE_COMPONENT_SUCCESS:
+      return { ...state, data: action.data, error: false, isFetching: false };
+    case actions.FETCHING_ONE_COMPONENT_FAILURE:
+      return { ...state, error: action.error, isFetching: false };
+    default:
+      return { ...state };
+  }
+};
