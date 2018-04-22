c2b1e8af51462e5028a58f790665d0ffb6578153
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-22 16:01:59 +0200

    Imported constants and replaced the string with the key from constants.js

--- Frontend-React/src/store/StateLoader.js
+++ Frontend-React/src/store/StateLoader.js
@@ -1,5 +1,7 @@
+import { reactStoreState } from "../constants";
+
 export const loadState = () =>
-  JSON.parse(localStorage.getItem("ReactStoreState")) || {};
+  JSON.parse(localStorage.getItem(reactStoreState)) || {};
 
 export const saveState = state =>
-  localStorage.setItem("ReactStoreState", JSON.stringify(state));
+  localStorage.setItem(reactStoreState, JSON.stringify(state));
