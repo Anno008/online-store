edf39952dceb0aa48bcf84705012e80448ce4632
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-26 19:35:24 +0200

    commit
    updated the apiUrl in constants.js

--- Frontend-React/src/constants.js
+++ Frontend-React/src/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = "http://localhost:8080/api";
+export const apiUrl = process.env.NODE_ENV == "production" ? "https://backon.herokuapp.com/api" : "http://localhost:8080/api";
 export const clientId = "react-frontend";
 export const reactStoreState = "reactStoreState";
