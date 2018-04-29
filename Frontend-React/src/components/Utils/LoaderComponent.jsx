7f39002048d2e636f1d0bfe0fa40bb6992533404
Aleksa Jovicic <aleksa.j@outlook.com>
2018-04-29 19:55:24 +0200

    Updated loader component

--- Frontend-React/src/components/Utils/LoaderComponent.jsx
+++ Frontend-React/src/components/Utils/LoaderComponent.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "components/css/loader.css";
 
 export const LoaderComponent = () => (
-  <div class="loaderContainer">
-    <div class="loader loader-1">
-      <div class="loader-outer" />
-      <div class="loader-inner" />
+  <div className="loaderContainer">
+    <div className="loader loader-1">
+      <div className="loader-outer" />
+      <div className="loader-inner" />
     </div>
   </div>
 );
