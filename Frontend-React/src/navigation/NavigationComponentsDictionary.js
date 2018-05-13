0dcfd543b4cde8d00f2d58af13dfbbc06e0b2c12
Aleksa Jovicic <aleksa.j@outlook.com>
2018-05-13 19:01:31 +0200

    Added shoppping cart to navigation dictionary

--- Frontend-React/src/navigation/NavigationComponentsDictionary.js
+++ Frontend-React/src/navigation/NavigationComponentsDictionary.js
@@ -3,11 +3,13 @@ import React from "react";
 import ComponentsCatalogComponent from "components/ShoppingComponents/ComponentsCatalogComponent";
 import AuthComponent from "../components/AuthComponent/AuthComponent";
 import ComponentDetailsComponent from "../components/ShoppingComponents/ComponentDetailsComponents/ComponentDetailsComponent";
+import ShoppingCartComponent from "../components/ShoppingCartComponents/ShoppingCartComponent";
 
 const components = {
   catalog: <ComponentsCatalogComponent />,
   auth: <AuthComponent />,
-  details: <ComponentDetailsComponent />
+  details: <ComponentDetailsComponent />,
+  cart: <ShoppingCartComponent/>
 };
 
 export const get = key => components[key] || console.log(`No components matching the key ${key}`);
