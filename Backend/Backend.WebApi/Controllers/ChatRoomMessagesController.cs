a3018f2ec1f3160aee8e104efddf02b22bc1c392
Aleksa Jovicic <aleksa.j@outlook.com>
2018-06-02 17:25:16 +0200

    Created ChatRoomMessagesController, will have to be updated to support paging on retreiving the messages, as not to get all at once

---
+++ Backend/Backend.WebApi/Controllers/ChatRoomMessagesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Backend.WebApi.Repositories;
+using Backend.WebApi.WebSocketRelatedStuff;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChatRoomMessagesController : Controller
+    {
+        private readonly ChatRoomRepository chatRoomRepository;
+
+        public ChatRoomMessagesController(ChatRoomRepository chatRoomRepository) =>
+            this.chatRoomRepository = chatRoomRepository;
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        public async Task<List<MessageData>> Get() =>
+            (await chatRoomRepository.GetAllAsync()).Select(m => new MessageData(m)).ToList();
+    }
+}
