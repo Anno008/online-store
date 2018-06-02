0f654969bad21bf75fbca5a4967362ebb364b9a2
Aleksa Jovicic <aleksa.j@outlook.com>
2018-06-02 12:39:10 +0200

    Created a chat room repository

---
+++ Backend/Backend.WebApi/Repositories/ChatRoomRepository.cs
@@ -0,0 +1,9 @@
+﻿using Backend.WebApi.Models;
+
+namespace Backend.WebApi.Repositories
+{
+    public class ChatRoomRepository : BaseRepository<ChatRoomMessage>
+    {
+        public ChatRoomRepository(DatabaseContext context) : base(context) { }
+    }
+}
