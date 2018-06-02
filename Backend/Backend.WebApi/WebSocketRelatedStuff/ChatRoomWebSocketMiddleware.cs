04790356f5a041653aced70fbc470a37dd996558
Aleksa Jovicic <aleksa.j@outlook.com>
2018-06-02 12:58:10 +0200

    Created web socket middleware

---
+++ Backend/Backend.WebApi/WebSocketRelatedStuff/ChatRoomWebSocketMiddleware.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Backend.WebApi.Models;
+using Backend.WebApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Backend.WebApi.WebSocketRelatedStuff
+{
+    public class ChatRoomWebSocketMiddleware
+    {
+        private static ConcurrentDictionary<string, WebSocket> _sockets = new ConcurrentDictionary<string, WebSocket>();
+
+        private readonly RequestDelegate _next;
+        private readonly ChatRoomRepository _chatRoomRepository;
+
+        public ChatRoomWebSocketMiddleware(RequestDelegate next, ChatRoomRepository chatRoomRepository)
+        {
+            _next = next;
+            _chatRoomRepository = chatRoomRepository;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.WebSockets.IsWebSocketRequest)
+            {
+                await _next.Invoke(context);
+                return;
+            }
+
+            CancellationToken ct = context.RequestAborted;
+            WebSocket currentSocket = await context.WebSockets.AcceptWebSocketAsync();
+            var socketId = Guid.NewGuid().ToString();
+
+            _sockets.TryAdd(socketId, currentSocket);
+
+            while (true)
+            {
+                if (ct.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                var response = await ReceiveStringAsync(currentSocket, ct);
+                var messageEntity = new ChatRoomMessage
+                {
+                    Date = response.Date,
+                    Message = response.Message,
+                    User = response.User
+                };
+
+                _chatRoomRepository.CreateAsync(messageEntity);
+                if (string.IsNullOrEmpty(response.Message))
+                {
+                    if (currentSocket.State != WebSocketState.Open)
+                    {
+                        break;
+                    }
+
+                    continue;
+                }
+
+                foreach (var socket in _sockets)
+                {
+                    if (socket.Value.State != WebSocketState.Open)
+                    {
+                        continue;
+                    }
+                    await SendStringAsync(socket.Value, JsonConvert.SerializeObject(response), ct);
+                }
+            }
+
+            _sockets.TryRemove(socketId, out WebSocket dummy);
+
+            await currentSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", ct);
+            currentSocket.Dispose();
+        }
+
+        private static Task SendStringAsync(WebSocket socket, string data, CancellationToken ct = default(CancellationToken))
+        {
+            var buffer = Encoding.UTF8.GetBytes(data);
+            var segment = new ArraySegment<byte>(buffer);
+            return socket.SendAsync(segment, WebSocketMessageType.Text, true, ct);
+        }
+
+        private static async Task<MessageData> ReceiveStringAsync(WebSocket socket, CancellationToken ct = default(CancellationToken))
+        {
+            var buffer = new ArraySegment<byte>(new byte[8192]);
+            using (var ms = new MemoryStream())
+            {
+                WebSocketReceiveResult result;
+                do
+                {
+                    ct.ThrowIfCancellationRequested();
+
+                    result = await socket.ReceiveAsync(buffer, ct);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+                }
+                while (!result.EndOfMessage);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                if (result.MessageType != WebSocketMessageType.Text)
+                {
+                    return null;
+                }
+                using (var reader = new StreamReader(ms, Encoding.UTF8))
+                {
+                    return JsonConvert.DeserializeObject<MessageData>(await reader.ReadToEndAsync());
+                }
+            }
+        }
+    }
+}
