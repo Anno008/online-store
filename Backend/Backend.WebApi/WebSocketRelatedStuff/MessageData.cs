f674a639b3e209befdbfb1ccef75ac054b661964
Aleksa Jovicic <aleksa.j@outlook.com>
2018-06-02 12:02:31 +0200

    Created MessageData class which will be the represented model of date which is sent/received via the web socket

---
+++ Backend/Backend.WebApi/WebSocketRelatedStuff/MessageData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Backend.WebApi.Models;
+using Newtonsoft.Json;
+
+namespace Backend.WebApi.WebSocketRelatedStuff
+{
+    public class MessageData
+    {
+        public MessageData()
+        {
+
+        }
+
+        public MessageData(ChatRoomMessage m)
+        {
+            Message = m.Message;
+            User = m.User;
+            Date = m.Date;
+        }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+    }
+}
