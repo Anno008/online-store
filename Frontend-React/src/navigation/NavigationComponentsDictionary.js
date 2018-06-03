9c22906d508c3b00aef97c1a6ae29ceb159c782e
Aleksa Jovicic <aleksa.j@outlook.com>
2018-06-03 17:31:55 +0200

    Added the chat room component to nav navigation components dictionary

--- Frontend-React/src/navigation/NavigationComponentsDictionary.js
+++ Frontend-React/src/navigation/NavigationComponentsDictionary.js
@@ -4,12 +4,14 @@ import ComponentsCatalogComponent from "components/ShoppingComponents/Components
 import AuthComponent from "../components/AuthComponent/AuthComponent";
 import ComponentDetailsComponent from "../components/ShoppingComponents/ComponentDetailsComponents/ComponentDetailsComponent";
 import ShoppingCartComponent from "../components/ShoppingCartComponents/ShoppingCartComponent";
+import ChatRoomComponent from "../components/ChatRoomComponents/ChatRoomComponent";
 
 const components = {
   catalog: <ComponentsCatalogComponent />,
   auth: <AuthComponent />,
   details: <ComponentDetailsComponent />,
-  cart: <ShoppingCartComponent/>
+  cart: <ShoppingCartComponent/>,
+  chat: <ChatRoomComponent />
 };
 
 export const get = key => components[key] || console.log(`No components matching the key ${key}`);
