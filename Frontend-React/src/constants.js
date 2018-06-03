c7622bdbfc757cbc4d5ca12c7c0d56c16050746c
Aleksa Jovicic <aleksa.j@outlook.com>
2018-06-03 19:41:09 +0200

    Updated the web socket endpoint for production with wss instead of ws, wss (web socket secure)

--- Frontend-React/src/constants.js
+++ Frontend-React/src/constants.js
@@ -1,4 +1,4 @@
 export const apiUrl = process.env.NODE_ENV == "production" ? "https://backon.herokuapp.com/api" : "http://localhost:8080/api";
-export const apiSocketUrl = process.env.NODE_ENV == "production" ? "ws://backon.herokuapp.com" : "ws://localhost:8080";
+export const apiSocketUrl = process.env.NODE_ENV == "production" ? "wss://backon.herokuapp.com" : "ws://localhost:8080";
 export const clientId = "react-frontend";
 export const reactStoreState = "reactStoreState";
