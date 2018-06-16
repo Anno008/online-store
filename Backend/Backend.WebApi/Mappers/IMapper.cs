b78fef17f844d0ee9805e7dd4662efc760583849
Aleksa Jovicic <aleksa.j@outlook.com>
2018-06-16 23:15:42 +0200

    Created a mapping interface

---
+++ Backend/Backend.WebApi/Mappers/IMapper.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.WebApi.Mappers
+{
+    public interface IMapper<TEntityModel, TDto>
+    {
+        TEntityModel ToEntityModel(TDto dto);
+        TDto ToDto(TEntityModel model);
+    }
+}
