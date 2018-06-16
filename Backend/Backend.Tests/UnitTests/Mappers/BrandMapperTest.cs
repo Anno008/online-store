44e6fc88e9393a054905f9ce7a7acfedc92b5ecd
Aleksa Jovicic <aleksa.j@outlook.com>
2018-06-16 23:28:15 +0200

    Created BrandDto, Brand Mapper and BrandMapper unit tests

---
+++ Backend/Backend.Tests/UnitTests/Mappers/BrandMapperTest.cs
@@ -0,0 +1,47 @@
+﻿using Backend.WebApi.DTOs;
+using Backend.WebApi.Mappers;
+using Backend.WebApi.Models;
+using Xunit;
+
+namespace Backend.Tests.UnitTests.Mappers
+{
+    public class BrandMapperTest
+    {
+        private readonly BrandMapper _brandMapper;
+
+        public BrandMapperTest()
+        {
+            _brandMapper = new BrandMapper();
+        }
+
+        [Fact]
+        public void DtoToModelEntityTest()
+        {
+            var dto = new BrandDto
+            {
+                Id = 1,
+                Name = "One"
+            };
+
+            var model = _brandMapper.ToEntityModel(dto);
+
+            Assert.Equal(dto.Id, model.Id);
+            Assert.Equal(dto.Name, model.Name);
+        }
+
+        [Fact]
+        public void ModelEntityToDtoTest()
+        {
+            var model = new Brand
+            {
+                Id = 1,
+                Name = "One"
+            };
+
+            var dto = _brandMapper.ToDto(model);
+
+            Assert.Equal(model.Id, dto.Id);
+            Assert.Equal(model.Name, dto.Name);
+        }
+    }
+}
---
+++ Backend/Backend.WebApi/Mappers/BrandMapper.cs
@@ -0,0 +1,26 @@
+﻿using Backend.WebApi.DTOs;
+using Backend.WebApi.Models;
+
+namespace Backend.WebApi.Mappers
+{
+    public class BrandMapper : IMapper<Brand, BrandDto>
+    {
+        public Brand ToEntityModel(BrandDto dto)
+        {
+            return new Brand
+            {
+                Id = dto.Id,
+                Name = dto.Name
+            };
+        }
+
+        public BrandDto ToDto(Brand model)
+        {
+            return new BrandDto
+            {
+                Id = model.Id,
+                Name = model.Name
+            };
+        }
+    }
+}
