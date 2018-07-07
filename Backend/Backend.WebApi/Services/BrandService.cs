c175897ce81d1fa6adf3a416dee8a28222651df2
Aleksa Jovicic <aleksa.j@outlook.com>
2018-07-07 20:51:24 +0200

    Created Brand Service

---
+++ Backend/Backend.WebApi/Services/BrandService.cs
@@ -0,0 +1,15 @@
+﻿using Backend.WebApi.DTOs;
+using Backend.WebApi.Mappers;
+using Backend.WebApi.Models;
+using Backend.WebApi.Repositories;
+
+namespace Backend.WebApi.Services
+{
+    public class BrandService : ServiceBase<Brand, BrandDto>
+    {
+        public BrandService(BrandRepository repository, IMapper<Brand, BrandDto> mapper) : base(repository, mapper)
+        {
+
+        }
+    }
+}
